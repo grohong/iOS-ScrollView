328bdd5a673889184bc7f1999f8aaec1b716b0ae
Seong ho Hong <grohong76@gmail.com>
2018-06-23 23:29:33 +0900

    [config]: set project

---
+++ PhotoScroll_Starter/PhotoScroll/PhotoCommentViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCommentViewController.swift
+//  PhotoScroll
+//
+//  Created by Seong ho Hong on 2018. 6. 23..
+//  Copyright © 2018년 raywenderlich. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCommentViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
